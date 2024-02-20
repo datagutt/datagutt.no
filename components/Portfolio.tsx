73dfae3c3548a61e7623a57c5965ddb233fadb08
Thomas Lekanger <thomas.lekanger@hotmail.com>
2024-02-20 18:38:08 +0100

    fix: mobile adjustment

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Image from "next/image";
@@ -10,6 +10,14 @@ import Link from "next/link";
 export default function Portfolio() {
   const containerRef = useRef(null);
   const showcaseRef = useRef(null);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, []);
 
   gsap.registerPlugin(ScrollTrigger);
 
@@ -22,12 +30,11 @@ export default function Portfolio() {
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: containerRef.current,
-          start: "top bottom",
+          start: isMobile ? "top center" : "top bottom",
           end: "bottom top",
           scrub: 0.5,
         },
       });
-
       tl.to(
         showcaseRef.current,
         {
@@ -41,13 +48,12 @@ export default function Portfolio() {
   }, []);
 
   return (
-    <section ref={containerRef} className="mb-[40vw]">
+    <section ref={containerRef} className="mb-[120vw] md:mb-[40vw]">
       <div
         ref={showcaseRef}
         className="relative mb-[5vw] z-40 md:w-[125.6vw] md:h-[34.6vw] h-[100vw]"
       >
-        <span className="sr-only">Portfolio coming soon</span>
-        <div className="flex flex-row w-full h-full gap-4 md:gap-8">
+        <div className="grid md:flex w-full h-full gap-4 md:gap-8 grid-cols-2 md:grid-cols-none">
           {projects.map((project) => (
             <Link
               key={project.id}
@@ -56,7 +62,7 @@ export default function Portfolio() {
               passHref
               className="relative w-full h-full"
             >
-              <div className="relative w-full h-[50vw] md:h-[17.3vw]">
+              <div className="relative w-full h-full md:h-[50vw] lg:h-[17.3vw]">
                 <Image
                   src={project.image}
                   alt={project.name}
@@ -65,11 +71,11 @@ export default function Portfolio() {
                   className="rounded-lg h-full w-full"
                 />
               </div>
-              <div className="absolute bottom-0 left-0 w-full h-1/2 bg-black bg-opacity-50 p-4">
+              <div className="absolute bottom-0 left-0 w-full md:h-1/2 bg-black bg-opacity-50 md:p-4">
                 <h2 className="text-white text-3xl md:text-4xl font-bit mb-2">
                   {project.name}
                 </h2>
-                <p className="text-white text-sm md:text-base font-light">
+                <p className="text-white text-sm md:text-base font-light hidden md:block">
                   {project.description}
                 </p>
               </div>
