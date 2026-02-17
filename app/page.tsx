6369d860d7a66f11864bac85c509b9b29e12f2b8
Thomas Lekanger <thomas.lekanger@hotmail.com>
2026-02-17 12:38:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
+import { Suspense } from "react";
+import { ClientLogger } from "@/components/client-logger";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 import ScrollText from "@/components/ScrollText";
-import { ClientLogger } from "@/components/client-logger";
-import { Suspense } from "react";
 
 export default async function Home() {
-  return (
-    <>
-      <main className="relative z-[10]">
-        <Suspense fallback={null}>
-          <Hero />
-        </Suspense>
-        <ScrollText />
-        <Portfolio />
-      </main>
-      <Suspense fallback={null}>
-        <Footer />
-      </Suspense>
-      <ClientLogger />
-    </>
-  );
+	return (
+		<>
+			<main className="relative z-[10]">
+				<Suspense fallback={null}>
+					<Hero />
+				</Suspense>
+				<Suspense fallback={null}>
+					<ScrollText />
+				</Suspense>
+				<Portfolio />
+			</main>
+			<Suspense fallback={null}>
+				<Footer />
+			</Suspense>
+			<ClientLogger />
+		</>
+	);
 }
